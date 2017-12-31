ad5d8ed1290d5ba1bd1765293f98a77d33b13489
akaustav <akaustav@gmail.com>
2017-12-30 21:59:08 -0700

    DashboardCardService with - Private _cards member - Constructor - Public addCard method - Public removeCard method - Public getter cards

--- src/app/dashboard/dashboard-cards.service.ts
+++ src/app/dashboard/dashboard-cards.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { DashboardCard } from './dashboard-card';
 
 @Injectable()
 export class DashboardCardsService {
 
-  constructor() { }
+  private _cards: BehaviorSubject<DashboardCard[]>;
+
+  constructor() {
+    this._cards = new BehaviorSubject<DashboardCard[]>([]);
+  }
+
+  public addCard(card: DashboardCard) {
+    const curCards: DashboardCard[] = this._cards.getValue();
+    const newCards: DashboardCard[] = curCards.concat(card);
+    this._cards.next(newCards);
+  }
+
+  public removeCard(card: DashboardCard) {
+    const curCards: DashboardCard[] = this._cards.getValue();
+    const index: number = curCards.indexOf(card);
+    if (index > -1) {
+      curCards.splice(index, 1);
+    }
+    this._cards.next(curCards);
+  }
+
+  public get cards(): BehaviorSubject<DashboardCard[]> {
+    return this._cards;
+  }
 
 }
