2c55f7965bcc2ccf5ff9999e8bf01d4043e2633f
akaustav <akaustav@gmail.com>
2017-12-30 23:19:40 -0700

    Updated Dashboard Model

--- src/app/dashboard/dashboard-card.ts
+++ src/app/dashboard/dashboard-card.ts
@@ -1,3 +1,33 @@
+import { InjectionToken } from '@angular/core';
+
+export interface InputPropertyInterface {
+  key: InjectionToken<string>;
+  value: string;
+}
+
+export interface InputInterface {
+  name: InputPropertyInterface;
+  routerLink: InputPropertyInterface;
+  color: InputPropertyInterface;
+}
+
 export class DashboardCard {
-  constructor() {}
+  static metadata: any = {
+    NAME: new InjectionToken<string>('name'),
+    ROUTERLINK: new InjectionToken<string>('routerLink'),
+    COLOR: new InjectionToken<string>('color')
+  };
+
+  constructor(
+    private _input: InputInterface,
+    private _component: any
+  ) {}
+
+  public get inputs(): any {
+    return this._input;
+  }
+
+  public get component(): any {
+    return this._component;
+  }
 }
