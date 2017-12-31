f334462fabe0d119d929322e488bb4716479adb2
akaustav <akaustav@gmail.com>
2017-12-30 20:49:55 -0700

    Creating initial DashboardCardsService

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { DashboardCardsService } from './dashboard-cards.service';
+
 @NgModule({
   declarations: [
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [DashboardCardsService]
 })
 export class DashboardModule { }
